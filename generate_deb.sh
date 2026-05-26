be4806e5ea4ff4366eba9f7f199b493e37c2602c
AfalpHy <736353503@qq.com>
2026-05-26 10:11:29 +0800

    chore: update generate_deb.sh

--- generate_deb.sh
+++ generate_deb.sh
@@ -1,62 +1,89 @@
 #!/bin/bash
 
 # ---------- CONFIG ----------
-APP_NAME="Sylvakru"           # Executable name
-APP_VERSION="3.0.0"               # Debian version
+BINARY_NAME="sylvakru"
+APP_NAME="Sylvakru"
+APP_VERSION="3.0.0"
 MAINTAINER="AfalpHy"
-EXECUTABLE="build/linux/x64/release/bundle/$APP_NAME"
-DATA="build/linux/x64/release/bundle/data"
-LIB="build/linux/x64/release/bundle/lib"
+
+BUNDLE_DIR="build/linux/x64/release/bundle"
+
+EXECUTABLE="$BUNDLE_DIR/$BINARY_NAME"
+DATA="$BUNDLE_DIR/data"
+LIB="$BUNDLE_DIR/lib"
+
 PACKAGE_DIR="build/deb_pkg"
-ICON_FILE="assets/app_icon.png"        # App icon
+
+ICON_FILE="assets/app_icon.png"
 # -----------------------------
 
 # Clean previous build
-rm -rf $PACKAGE_DIR
-mkdir -p $PACKAGE_DIR/DEBIAN
-mkdir -p $PACKAGE_DIR/usr/local/bin
-mkdir -p $PACKAGE_DIR/usr/share/icons/hicolor/128x128/apps
-mkdir -p $PACKAGE_DIR/usr/share/applications
+rm -rf "$PACKAGE_DIR"
 
-# Copy binary
-cp "$EXECUTABLE" "$PACKAGE_DIR/usr/local/bin/$APP_NAME"
-cp "$DATA" -r "$PACKAGE_DIR/usr/local/bin/"
-cp "$LIB" -r "$PACKAGE_DIR/usr/local/bin/"
+# Create directories
+mkdir -p "$PACKAGE_DIR/DEBIAN"
 
-# Auto-detect dependencies
-echo "Detecting dependencies..."
-DEPENDS="libc6 (>= 2.31), libstdc++6, libgtk-3-0, libayatana-appindicator3-1, libdbusmenu-gtk3-4, libx11-6, libxext6, libxrender1, libwayland-client0, libwayland-cursor0, libwayland-egl1-mesa, libxkbcommon0, libmpv-dev"
-echo "Detected dependencies: $DEPENDS"
+# App bundle location
+mkdir -p "$PACKAGE_DIR/usr/lib/$BINARY_NAME"
 
-# Create control file
+# Launcher
+mkdir -p "$PACKAGE_DIR/usr/bin"
+
+# Desktop integration
+mkdir -p "$PACKAGE_DIR/usr/share/icons/hicolor/128x128/apps"
+mkdir -p "$PACKAGE_DIR/usr/share/applications"
+
+# Copy Flutter bundle
+cp "$EXECUTABLE" "$PACKAGE_DIR/usr/lib/$BINARY_NAME/"
+cp -r "$DATA" "$PACKAGE_DIR/usr/lib/$BINARY_NAME/"
+cp -r "$LIB" "$PACKAGE_DIR/usr/lib/$BINARY_NAME/"
+
+# Create launcher script
+cat > "$PACKAGE_DIR/usr/bin/$BINARY_NAME" <<EOL
+#!/bin/sh
+exec /usr/lib/$BINARY_NAME/$BINARY_NAME "\$@"
+EOL
+
+chmod 755 "$PACKAGE_DIR/usr/bin/$BINARY_NAME"
+chmod 755 "$PACKAGE_DIR/usr/lib/$BINARY_NAME/$BINARY_NAME"
+
+# Runtime dependencies
+DEPENDS="libc6 (>= 2.31), libstdc++6, libgtk-3-0, libayatana-appindicator3-1, libdbusmenu-gtk3-4, libmpv2"
+
+# Control file
 cat > "$PACKAGE_DIR/DEBIAN/control" <<EOL
-Package: $APP_NAME
+Package: $BINARY_NAME
 Version: $APP_VERSION
-Section: base
+Section: utils
 Priority: optional
 Architecture: amd64
 Depends: $DEPENDS
 Maintainer: $MAINTAINER
-Description: $APP_NAME Flutter Linux app
+Description: $BINARY_NAME Flutter Linux app
  A Flutter desktop application for Linux.
 EOL
 
 # Copy icon
-cp "$ICON_FILE" "$PACKAGE_DIR/usr/share/icons/hicolor/128x128/apps/$APP_NAME.png"
+cp "$ICON_FILE" \
+"$PACKAGE_DIR/usr/share/icons/hicolor/128x128/apps/$BINARY_NAME.png"
 
-# Create desktop entry
-cat > "$PACKAGE_DIR/usr/share/applications/$APP_NAME.desktop" <<EOL
+# Desktop entry
+cat > "$PACKAGE_DIR/usr/share/applications/$BINARY_NAME.desktop" <<EOL
 [Desktop Entry]
 Name=$APP_NAME
-Exec=/usr/local/bin/$APP_NAME
-Icon=$APP_NAME
+Exec=$BINARY_NAME
+Icon=$BINARY_NAME
 Type=Application
 Categories=Utility;
-StartupWMClass=Com.afalphy.sylvakru
+StartupWMClass=com.afalphy.sylvakru
+Terminal=false
 EOL
 
-# Build .deb
-dpkg-deb --build "$PACKAGE_DIR"
-mv "${PACKAGE_DIR}.deb" "build/${APP_NAME}-${APP_VERSION}-linux-amd64.deb"
+# Build package
+dpkg-deb --build --root-owner-group "$PACKAGE_DIR"
 
-echo "Debian package created: build/${APP_NAME}-${APP_VERSION}-linux-amd64.deb"
+mv "${PACKAGE_DIR}.deb" \
+"build/${BINARY_NAME}-${APP_VERSION}-linux-amd64.deb"
+
+echo "Debian package created:"
+echo "build/${BINARY_NAME}-${APP_VERSION}-linux-amd64.deb"
